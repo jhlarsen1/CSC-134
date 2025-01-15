604f852e40cb1256e1b08143952a3cecee903ef9
jhlarsen1 <jhlarsen@live.com>
2025-01-15 16:17:42 +0000

    ran code it works

---
+++ hello.cpp
@@ -0,0 +1,22 @@
+//CSC 134
+//Hello World
+//LarsenJ
+//1/15/25
+
+/*
+or you can
+do multi line
+comments
+*/
+
+//include lets us use input and output
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   cout << "Hello, World!" << endl;
+   
+   return 0;
+}
