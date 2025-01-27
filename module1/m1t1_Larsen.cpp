b2deb12ee5c2cba23c0fe68e5302ef8da334bd58
jhlarsen1 <jhlarsen@live.com>
2025-01-27 16:13:55 +0000

    wrote ashort program, to catch up in class

---
+++ module1/m1t1_Larsen.cpp
@@ -0,0 +1,14 @@
+//CSC -134
+//M1t1 - Hellow World
+//LarsenJ
+//1/27/2025
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello";
+    cout << endl;
+    return 0;
+}
---
+++ module1/m1t2_Larsen.cpp
@@ -0,0 +1,17 @@
+//CSC -134
+//M1t1 - Hellow World
+//LarsenJ
+//1/27/2025
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello, ";
+    cout << "my name is Justin and I enjoy anime and gaming.";
+    cout << endl;
+    cout << "Best anime out right now is definitley Solo Leveling";
+    cout << endl;
+    return 0;
+}
