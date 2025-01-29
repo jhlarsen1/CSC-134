dffa42d96e48b01846d40e611bca5f8f1a950654
jhlarsen1 <jhlarsen@live.com>
2025-01-29 15:43:13 +0000

    added variables to m2t1

--- module2/m2t1_Larsen.cpp
+++ module2/m2t1_Larsen.cpp
@@ -2,7 +2,8 @@
 //m2t1 the apple orchard part 2
 //LarsenJ
 //1/29/2025
-//hange to the original m1lab1 program
+//change to the original m1lab1 program
+//adding input to the original program
 
 
 #include <iostream>
@@ -14,9 +15,16 @@ int main()
     //Apple Sales program
     //variables are like mailboxes
     string name = "Larsen";
+    string user_name;
     int num_apples = 100;
     double cost_each = 0.25;
+//greeting
+    cout << "Hello whats your name? ";
+    cin >> user_name;
+    cout << "Thanks for coming by, " << user_name << ;
 
+
+//adding new variables
     cout << "Welcome to the " << name << " apple farm!" << endl;
     cout << "There are " << num_apples << " apples in stock" << endl;
     cout << "They cost $" << cost_each << " each." << endl;
