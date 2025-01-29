0d8ebc89b84671888f8c469bf52d8f111af9e3ab
jhlarsen1 <jhlarsen@live.com>
2025-01-29 16:14:55 +0000

    finished logic for m2t1

--- module2/m2t1_Larsen.cpp
+++ module2/m2t1_Larsen.cpp
@@ -14,17 +14,21 @@ int main()
 {
     //Apple Sales program
     //variables are like mailboxes
-    string name = "Larsen";
-    string user_name;
+    string name = "Larsen";   
     int num_apples = 100;
     double cost_each = 0.25;
+//adding new variables
+    string user_name;
+    int apples_to_buy;
+    
+
 //greeting
     cout << "Hello whats your name? ";
     cin >> user_name;
-    cout << "Thanks for coming by, " << user_name << ;
+    cout << "Thanks for coming by, " << user_name << "!" << endl;
 
 
-//adding new variables
+
     cout << "Welcome to the " << name << " apple farm!" << endl;
     cout << "There are " << num_apples << " apples in stock" << endl;
     cout << "They cost $" << cost_each << " each." << endl;
@@ -32,6 +36,14 @@ int main()
     double total_cost = num_apples * cost_each;
     cout << fixed << setprecision(2);
     cout << "The price for all the apples is: $" << total_cost <<endl;
+    
+    cout << "How many apples would you like to buy? "; //changes for m2t1
+    cin >> apples_to_buy;
+
+    //logic for new math
+    double new_total_cost = apples_to_buy * cost_each; //chamges for m2t1
+    
+    cout << "The price for the apples you would like to buy is $" << new_total_cost <<endl; //changes for m2t1
 
     return 0;
 }
