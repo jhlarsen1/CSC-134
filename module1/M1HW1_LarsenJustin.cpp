005eca25b5b8ab0746d21146f09c012f64bd0992
jhlarsen1 <jhlarsen@live.com>
2025-02-08 20:50:34 +0000

    finished code for m1hw1

--- module1/M1HW1_LarsenJustin.cpp
+++ module1/M1HW1_LarsenJustin.cpp
@@ -8,3 +8,20 @@
 #include <iostream>
 #include <iomanip>
 using namespace std;
+
+int main()
+{
+
+    string movieName = "The Matrix";
+    string movieYear = "1999";
+    double movieGross = 467.2;
+
+    cout << "The movie " << movieName << " came out in " << movieYear << 
+    " and had a worldwide gross of around " << movieGross << " billion dollars. " << endl;
+
+    cout << "HERE ARE SOME COOL LINES FROM THE MOVIE:" << endl;
+    cout << "Neo: \"I know kung fu.\"" << endl;
+    cout << "Agent Smith: \"Human beings are a disease, a cancer of this planet. You're a plague, and we are the cure.\"" <<endl;    
+    cout << "Morpheus: \"There’s a difference between knowing the path and walking the path.\"" << endl;
+    cout << "Neo: \"My name… is Neo.\"" << endl;
+}
