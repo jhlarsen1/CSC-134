f1aefe3fc7fdac892fcded0423d7f6450ab802f0
jhlarsen1 <jhlarsen@live.com>
2025-02-09 22:28:29 +0000

    started getting user input

--- module2/m2lab1_Larsen.cpp
+++ module2/m2lab1_Larsen.cpp
@@ -6,3 +6,28 @@
 
 #include <iostream>
 
+using namespace std;
+
+const double COST_PER_CUBIC_FOOT = 0.23;
+const double CHARGE_PER_CUBIC_FOOT = 0.5;
+double xLength;
+double xWidth;
+double xHeight;
+double xVolume;
+double xCost;
+double xCharge;
+double xProfit;
+
+
+int main()
+{
+
+    cout << "Enter crate's length:  ";
+    cin >> xLength;
+
+    cout <<"Enter crate's width:  ";
+    cin >> xWidth;
+
+
+}
+
