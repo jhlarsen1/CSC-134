e6bae4692ec2935cbf6be5e1901ef3954ae7e6f4
jhlarsen1 <jhlarsen@live.com>
2025-03-17 15:28:49 +0000

    changed starting to code to say hello 5 times

---
+++ module4/m4t1_LarsenJ.cpp
@@ -0,0 +1,24 @@
+/*
+CSC 134
+m4t1
+LarsenJ
+3/17/2024
+
+*/
+
+#include <iostream>
+using namespace std;
+  
+int main()
+{
+
+    // Part 1, just say "hi" five times
+    int count = 1;
+    while (count <= 5) 
+    {
+        cout << "Hello number " << count << endl;
+        count++;
+    }
+
+    return 0; // done
+}
