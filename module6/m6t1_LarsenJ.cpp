31dd59bef68d018a240ceede199d9add7c4355d1
jhlarsen1 <jhlarsen@live.com>
2025-04-07 15:03:56 +0000

    added code for array

--- module6/m6t1_LarsenJ.cpp
+++ module6/m6t1_LarsenJ.cpp
@@ -14,19 +14,28 @@ int main()
     int cars_today;
     int cars_total = 0;
     double cars_average;
+    int cars[5];
 
     cout <<"Enter cars that passs for each day." << endl;
     for (int i=0; i < DAYS; i++)
     {
         cout << "Day " << i+1 << ": " ;
         cin >> cars_today;
+        cars[i] = cars_today; //save todays value into array
         cars_total += cars_today;
 
     }
 
     cout << "Over " << DAYS << " days, total cars = " << cars_total << endl;
     cars_average = (double)cars_total / DAYS;
-    cout << "for an average of " << "each day." << endl;
+    cout << "for an average of " << cars_average << "each day." << endl;
+
+    //now print the array
+    for (int i = 0; i < DAYS; i++)
+    {
+        cout << "Day: " << i+1 << "Count: " << cars[i] << endl;
+    }
+
 
     
 }
